4e395b5788e619d906cc1b0433c07f7c337acb8d
tatsuya.satomura <the.krktty@gmail.com>
2020-02-04 12:40:37 +0000

    re add colmn

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->status = $request->status;
@@ -91,6 +95,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->status = $request->status;
